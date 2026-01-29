a5450029b9429566223de93e75c01cee6e850114
unknown <tienquy011295@gmail.com>
2026-01-30 03:09:19 +0700

    add PATCH to securityConfig

--- src/main/java/org/example/be_eproject_sem4/Config/SecurityConfig.java
+++ src/main/java/org/example/be_eproject_sem4/Config/SecurityConfig.java
@@ -85,7 +85,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOriginPatterns(List.of("http://localhost:3000", "http://localhost:5173")); // React, Vue...
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true); // quan trọng để cookie được gửi
 
